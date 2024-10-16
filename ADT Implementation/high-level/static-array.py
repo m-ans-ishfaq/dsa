eb0a667f73f8be03b98c14df63e5770c5f412b26
Ans <anasashfaq00@gmail.com>
2024-10-16 21:08:31 +0500

    Fixed bug in static array

--- ADT Implementation/high-level/static-array.py
+++ ADT Implementation/high-level/static-array.py
@@ -2,6 +2,10 @@ class StaticArray:
   def __init__(self, size):
     self.arr = [None] * size
   def get(self, index):
-    return self.arr[self.index]
+    return self.arr[index]
   def set(self, index, val):
     self.arr[index] = val
+
+arr = StaticArray(10)
+arr.set(1,5)
+print(arr.get(1))
