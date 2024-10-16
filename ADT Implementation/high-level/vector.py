627b06afc8b236aa75389ad17df65cdddd7c481b
Ans <anasashfaq00@gmail.com>
2024-10-16 21:17:59 +0500

    Vector Implementation

---
+++ ADT Implementation/high-level/vector.py
@@ -0,0 +1,30 @@
+class Vector:
+  def __init__(self):
+    self.capacity = 1
+    self.length = 0
+    self.arr = [None]
+
+  def size(self):
+    return self.length
+  
+  def push(self, element):
+    if self.length == self.capacity:
+      self.capacity *= 2
+      new_arr = [None] * self.capacity
+      for i in range(self.length):
+        new_arr[i] = self.arr[i]
+      self.arr = new_arr
+    self.arr[self.length] = element
+    self.length += 1
+  
+  def display(self):
+    for x in self.arr:
+      if x != None:
+        print(x)
+
+v = Vector()
+v.push(1)
+v.push(2)
+v.push(5)
+v.push(6)
+v.display()
